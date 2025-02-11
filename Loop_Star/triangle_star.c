2e792056776cacce05240df6023cf2974be581ab
sujinee01 <kmosujin@gmail.com>
2025-02-11 09:01:50 +0900

    Upload triangle_star

---
+++ Loop_Star/triangle_star.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 0;
+    
+    for (; i < 6; i++) {
+        for(int j; j=<i/2; j++)
+            printf(" ");
+            if (j>i/2)
+                for(;j=<i;j++) {
+                    printf("*")
+            else (
+                for(int k; k=<i/2; k++)
+                printf(" ");
+            )
+            }
+    }
+
+    return 0;
+}
